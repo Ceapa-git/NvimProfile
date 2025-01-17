bf38219e6ab4674502efce4dfa397f7d93c51846
Ceapa-git <dan.bugnariu0902@gmail.com>
2025-01-18 00:26:14 +0200

    transparent bg

--- lua/dan/plugins/colors.lua
+++ lua/dan/plugins/colors.lua
@@ -1,1 +1,17 @@
-return ({ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 })
+return {
+	"bluz71/vim-moonfly-colors",
+	name = "moonfly",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme moonfly")
+		-- Make background transparent
+		vim.cmd([[
+            highlight Normal guibg=NONE ctermbg=NONE
+            highlight NonText guibg=NONE ctermbg=NONE
+            highlight LineNr guibg=NONE ctermbg=NONE
+            highlight SignColumn guibg=NONE ctermbg=NONE
+            highlight EndOfBuffer guibg=NONE ctermbg=NONE
+        ]])
+	end,
+}
